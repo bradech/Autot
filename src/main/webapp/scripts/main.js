2362307efacfc8d307078710cfb93f5d9404beec
christian.brade <christian.brade@haaga-helia.fi>
2022-09-30 17:30:24 +0300

    eka versio

---
+++ src/main/webapp/scripts/main.js
@@ -0,0 +1,27 @@
+//funktio tietojen hakemista varten. Kutsutaan backin GET metodia
+function haeAutot() {
+	let url = "autot"; 
+	let requestOptions = {
+        method: "GET",
+        headers: { "Content-Type": "application/x-www-form-urlencoded" }       
+    };    
+    fetch(url, requestOptions)
+    .then(response => response.json())//Muutetaan vastausteksti JSON-objektiksi 
+   	.then(response => printItems(response)) 
+   	.catch(errorText => console.error("Fetch failed: " + errorText));
+}
+
+//Kirjoitetaan tiedot taulukkoon JSON-objektilistasta
+function printItems(respObjList){
+	//console.log(respObjList);
+	let htmlStr="";
+	for(let item of respObjList){//yksi kokoelmalooppeista		
+    	htmlStr+="<tr id='rivi_"+item.id+"'>";
+    	htmlStr+="<td>"+item.rekno+"</td>";
+    	htmlStr+="<td>"+item.merkki+"</td>";
+    	htmlStr+="<td>"+item.malli+"</td>";
+    	htmlStr+="<td>"+item.vuosi+"</td>";     	
+    	htmlStr+="</tr>";    	
+	}	
+	document.getElementById("tbody").innerHTML = htmlStr;	
+}
